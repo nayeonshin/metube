1064b8a2c926bdbbc2f85e391ea2fa980a453bc1
Nayeon Shin <nayeon.codes@gmail.com>
2022-05-10 18:33:42 -0600

    feat: create `urlMiddleware`

--- src/server.js
+++ src/server.js
@@ -4,16 +4,16 @@ const PORT = 4000;
 
 const app = express();
 
+const urlMiddleware = (req, res, next) => {
+  console.log(`Someone is going to: ${req.url}`);
+  next();
+};
+
 const handleHome = (req, res) => {
   return res.send("Hello, World!");
 };
 
-const handleSignIn = (req, res) => {
-  return res.send("Sign in here.");
-};
-
-app.get("/", handleHome);
-app.get("/signin", handleSignIn);
+app.get("/", urlMiddleware, handleHome);
 
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT}`);
