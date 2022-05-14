e8e0bdfdaf71f6446d06999a16b2f0fb3d0a8649
Nayeon Shin <nayeon.codes@gmail.com>
2022-05-14 10:17:44 +0900

    feat: add routers for the root, user, and video

--- src/server.js
+++ src/server.js
@@ -6,12 +6,29 @@ const PORT = 4000;
 const app = express();
 const logger = morgan("dev");
 
-const handleHome = (req, res) => res.send("Hello!");
-const handleSignIn = (req, res) => res.send("Sign in");
-
 app.use(logger);
-app.get("/", handleHome);
-app.get("/signin", handleSignIn);
+
+const globalRouter = express.Router();
+
+const handleHome = (req, res) => res.send("Home");
+
+globalRouter.get("/", handleHome);
+
+const userRouter = express.Router();
+
+const handleEditUser = (req, res) => res.send("Edit user");
+
+userRouter.get("/edit", handleEditUser);
+
+const videoRouter = express.Router();
+
+const handleWatchVideo = (req, res) => res.send("Watch video");
+
+videoRouter.get("/watch", handleWatchVideo);
+
+app.use("/", globalRouter);
+app.use("/user", userRouter);
+app.use("/video", videoRouter);
 
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT}`);
