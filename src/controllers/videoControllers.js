b3fd97e781941936d470b155a5bddee73907bce9
Nayeon Shin <contact@nayeonshin.dev>
2022-05-19 12:00:32 +0900

    temp(`videoControllers`): print `req.params`

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -1,6 +1,14 @@
-export const deleteVideo = (req, res) => res.send("Delete");
-export const edit = (req, res) => res.send("Edit");
+export const deleteVideo = (req, res) => {
+  console.log(req.params);
+  return res.send("Delete");
+};
+export const edit = (req, res) => {
+  console.log(req.params);
+  return res.send("Edit");
+};
 export const search = (req, res) => res.send("Search");
-export const see = (req, res) => res.send("Watch");
+export const see = (req, res) => {
+  return res.send(`Watch Video #${req.params.id}`);
+};
 export const showAll = (req, res) => res.send("Homepage videos");
 export const upload = (req, res) => res.send("Upload");
