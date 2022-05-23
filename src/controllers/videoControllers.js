6f77ad87b2585d7316b9f6f6ecd18ca37130c81c
Nayeon Shin <contact@nayeonshin.dev>
2022-05-23 16:44:38 +0900

    feat: create video mixin under `mixins/`

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -6,7 +6,32 @@ export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
 export const watch = (req, res) => res.render("watch");
 export const showAll = (req, res) => {
-  const videos = [];
+  const videos = [
+    {
+      title: "Video #1",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
+    },
+    {
+      title: "Video #2",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
+    },
+    {
+      title: "Video #3",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
+    },
+  ];
   return res.render("home", { pageTitle: "Home", videos });
 };
 export const upload = (req, res) => res.send("Upload");
