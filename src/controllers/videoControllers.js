13c47a5ed5ce53a80bd3aee80cc35bbb30f00d2d
Nayeon Shin <contact@nayeonshin.dev>
2022-05-23 16:28:01 +0900

    feat(`home.pug`): iterate over `videos`

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -5,5 +5,8 @@ export const deleteVideo = (req, res) => {
 export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
 export const watch = (req, res) => res.render("watch");
-export const showAll = (req, res) => res.render("home", { pageTitle: "Home" });
+export const showAll = (req, res) => {
+  const videos = [];
+  return res.render("home", { pageTitle: "Home", videos });
+};
 export const upload = (req, res) => res.send("Upload");
